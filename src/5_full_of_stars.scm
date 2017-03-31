b874d260a3f0096a77e28db122ba7311386bc520
Ian R. Taylor <iantay@upenn.edu>
2017-03-31 10:40:58 -0400

    Add insertR* and additional notes

--- src/5_full_of_stars.scm
+++ src/5_full_of_stars.scm
@@ -17,3 +17,21 @@
 ; rember* can work on lists of lists, because it treats each element
 ; of the list as a list. of each element, it asks: "is this null?", "is this an atom?"
 ; see the first commandment!
+
+; thinking more about lists:
+(define l '(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce)))
+(car (car (car l)))        ; returns 'tomato
+(car (car (car (cdr l))))  ; returns 'bean
+
+; ... carry on...
+(define insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+        (cond
+          ((eq? (car l) old)
+            (cons old (cons new (insertR* new old (cdr l)))))
+        (else (cons (car l) (insertR* new old (cdr l))))))
+    (else (cons (insertR* new old (car l))          ; notice how the recursion splits into two branches
+                (insertR* new old (cdr l)))))))     ; 1) recurring down (car l) and 2) recurring down (cdr l)
