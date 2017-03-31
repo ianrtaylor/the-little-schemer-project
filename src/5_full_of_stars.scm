aa16809c570235549ff9c0b0e3abd6730814a631
Ian R. Taylor <iantay@upenn.edu>
2017-03-31 10:19:37 -0400

    add rember* with description

--- src/5_full_of_stars.scm
+++ src/5_full_of_stars.scm
@@ -1,1 +1,19 @@
-; getting to work on S-expression recursion
+; rember* vs rember
+; test using (define l '((coffee) cup ((tea) cup) (and (hick)) cup))
+(define rember*
+  (lambda (a l)
+    (cond
+      ((null? l) (quote ()))             ; is first element of l null? if so, return null
+      ((atom? (car l))                   ; is first element of l an atom?
+        (cond
+          ((eq? (car l) a)                  ; if so, is it eqal to a?
+            (rember* a (cdr l)))            ; then recur on the remainder of l, removing a
+          (else (cons (car l)               ; otherwise, begin building new list
+            (rember* a (cdr l))))))
+    (else (cons (rember* a (car l))     ; if first element of l is neither null nor an atom...
+        (rember* a (cdr l)))))))        ; then build new list...
+
+; regarding the above:
+; rember* can work on lists of lists, because it treats each element
+; of the list as a list. of each element, it asks: "is this null?", "is this an atom?"
+; see the first commandment!
