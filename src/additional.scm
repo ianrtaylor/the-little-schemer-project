5c413b437bae873e36aa724ebeaa53e0bd764121
Ian R. Taylor <iantay@upenn.edu>
2017-05-02 09:08:24 -0400

    Add defintion of S-expression

--- src/additional.scm
+++ src/additional.scm
@@ -5,3 +5,9 @@
 (define l '(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce)))
 (car (car (car l)))        ; returns 'tomato
 (car (car (car (cdr l))))  ; returns 'bean
+
+
+; Chapter 5
+; Definition of an S-Expression (p 92):
+;   An S-expression is either an atom or a
+;   (possibly empty) list of S-expressions.
