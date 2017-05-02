e0fbfbf486e7ca513c4bc2aac2d3180108fb951a
Ian R. Taylor <iantay@upenn.edu>
2017-05-02 09:30:29 -0400

    Add intro notes for ch 6

---
+++ src/6_shadows.scm
@@ -0,0 +1,9 @@
+; Chapter 6: Shadows
+
+; What is an arithmetic expression?
+;   1 + 3 is, but (1 + 3) is only a REPRESENTATION of an arithmetic
+;   expression, b/c it has parens.
+;   really, (1 + 3) is an S-Expression, which represents 1 + 3.
+
+; numbered? determines is an arithmetic expression representation
+;   contains only numbers, +, x, and ^
