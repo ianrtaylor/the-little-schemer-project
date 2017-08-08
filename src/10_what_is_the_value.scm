eccca115a8c2d8224cef59c60332aef46af2503d
Ian R. Taylor <iantay@upenn.edu>
2017-08-08 10:45:50 -0400

    Begin ch 10

---
+++ src/10_what_is_the_value.scm
@@ -0,0 +1,9 @@
+; Chapter 10: What Is The Value of All of This?
+
+; Entry: A pair of lists whose first list is a set (i.e. no atom occurs more than once)
+; Additionally, both lists must be of equal length
+
+; build an entry from a set of names and a list of values:
+(define new-entry build)
+
+; 
